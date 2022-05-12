3ed34ee204d8dd921dfc8aab0a0aab6048b44190
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-12 17:23:31 +0300

    Add files via upload

---
+++ My_first_project - Lab/George_Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArchitectProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = (Console.ReadLine());
+            int project = int.Parse(Console.ReadLine());
+            int hoursneed = 3;
+            int totalhoursneed = hoursneed * project;
+            Console.WriteLine($"The architect {name} will need {totalhoursneed} hours to complete {project} project/s.");
+        }
+    }
+}
---
+++ My_first_project - Lab/Greening/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMetersGreening = double.Parse(Console.ReadLine());
+
+            double finalPrice = 7.61 * squareMetersGreening;
+            double discount = 0.18 * finalPrice;
+            double finalPriceWithDiscount = finalPrice - discount;
+
+            Console.WriteLine($"The final price is: {finalPriceWithDiscount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
