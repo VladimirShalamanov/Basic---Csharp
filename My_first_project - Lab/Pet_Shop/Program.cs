625d38d52d6dd1c8603058c108cf08d6b4f7c33b
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-12 17:26:42 +0300

    Add files via upload

---
+++ My_first_project - Lab/Pet_Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double totalDogPrice = 2.50 * dogFood;
+            double totalCatPrice = 4.00 * catFood;
+
+            Console.WriteLine(totalDogPrice + totalCatPrice + " lv.");
+        }
+    }
+}
---
+++ My_first_project - Lab/Лице_на_правоъгълник/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Лице_на_правоъгълник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int area = a * b;
+            Console.WriteLine(area);
+
+        }
+    }
+}
