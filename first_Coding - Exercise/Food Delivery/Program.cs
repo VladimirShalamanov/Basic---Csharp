8cd626e39fdbf3cf9b161c20bff6059dd9060fac
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-13 23:11:26 +0300

    Add files via upload

---
+++ first_Coding - Exercise/Food Delivery/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int veganMenu = int.Parse(Console.ReadLine());
+
+            double priceChickan = chickenMenu * 10.35;
+            double priceFish = fishMenu * 12.40;
+            double priceVegan = veganMenu * 8.15;
+
+            double menuPrice = priceChickan + priceFish + priceVegan;
+            double dessertPrice = menuPrice * 0.2;
+            double deliveryPrice = 2.50;
+
+            double sum = menuPrice + dessertPrice + deliveryPrice;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ first_Coding - Exercise/Repainting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityNylon = int.Parse(Console.ReadLine());
+            int quantityPaint = int.Parse(Console.ReadLine());
+            int quantityDiluent = int.Parse(Console.ReadLine());
+            int workingHours = int.Parse(Console.ReadLine());
+
+            double priceBag = 0.40;
+            double priceNylon = quantityNylon * 1.50 + 2 * 1.50;
+            double pricePaint = (quantityPaint * 14.50 + 0.10 * quantityPaint * 14.50);
+            double priceDiluent = quantityDiluent * 5;
+
+            double pricePerHour = 0.30 * (priceBag + priceNylon + pricePaint + priceDiluent);
+            double priceWorkers = workingHours * pricePerHour;
+
+            double expenses = priceBag + priceNylon + pricePaint + priceDiluent + priceWorkers;
+            Console.WriteLine(expenses);
+        }
+    }
+}
