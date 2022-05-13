29fd06f9f31eccef4df546f972c12aed78c99a3e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-13 23:12:42 +0300

    Add files via upload

---
+++ first_Coding - Exercise/Supplies for School/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPens = int.Parse(Console.ReadLine());
+            int countMarkers = int.Parse(Console.ReadLine());
+            int litersPrep = int.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double pricePens = countPens * 5.80;
+            double priceMarkers = countMarkers * 7.20;
+            double pricePrep = litersPrep * 1.20;
+
+            double totall = pricePens + priceMarkers + pricePrep;
+
+            totall = totall - (percent / 100.0) * totall;
+
+            Console.WriteLine(totall);
+        }
+    }
+}
