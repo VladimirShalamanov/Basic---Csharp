095a8880c28e6164f4c7b501a19ab35718055048
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-15 17:51:11 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/06. Speed Info/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Speed_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+
+            if (number > 10 && number <= 50)
+            {
+                Console.WriteLine("average");
+            }
+
+            if (number > 50 && number <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+
+            if (number > 150 && number <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+
+            if (number > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
