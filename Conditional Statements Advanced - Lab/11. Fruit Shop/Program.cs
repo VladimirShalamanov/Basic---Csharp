fa741cd7f249e0de47f1dd505a0c03ae4d8d012a
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-17 16:02:57 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/11. Fruit Shop/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _11._Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruid = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quality = double.Parse(Console.ReadLine());
+
+            double totalSum = 0;
+            double price = 0;
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+                if (fruid == "banana")
+                {
+                    price = 2.50;
+                }
+                else if (fruid == "apple")
+                {
+                    price = 1.20;
+                }
+                else if (fruid == "orange")
+                {
+                    price = 0.85;
+                }
+                else if (fruid == "grapefruit")
+                {
+                    price = 1.45;
+                }
+                else if (fruid == "kiwi")
+                {
+                    price = 2.70;
+                }
+                else if (fruid == "pineapple")
+                {
+                    price = 5.50;
+                }
+                else if (fruid == "grapes")
+                {
+                    price = 3.85;
+                }
+            }
+
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                if (fruid == "banana")
+                {
+                    price = 2.70;
+                }
+                else if (fruid == "apple")
+                {
+                    price = 1.25;
+                }
+                else if (fruid == "orange")
+                {
+                    price = 0.90;
+                }
+                else if (fruid == "grapefruit")
+                {
+                    price = 1.60;
+                }
+                else if (fruid == "kiwi")
+                {
+                    price = 3.00;
+                }
+                else if (fruid == "pineapple")
+                {
+                    price = 5.60;
+                }
+                else if (fruid == "grapes")
+                {
+                    price = 4.20;
+                }
+            }
+
+            if (price == 0)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                totalSum = price * quality;
+                Console.WriteLine($"{totalSum:f2}");
+            }
+        }
+    }
+}
---
+++ Conditional Statements Advanced - Lab/12. Trade Commissions/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sale = double.Parse(Console.ReadLine());
+
+            double commissions = 0;
+
+            if (0 <= sale && sale <= 500)
+            {
+                if (city == "Sofia")
+                {
+                    commissions = sale * 0.05;
+                }
+                else if (city == "Varna")
+                {
+                    commissions = sale * 0.045;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commissions = sale * 0.055;
+                }
+            }
+
+            else if (500 < sale && sale <= 1000)
+            {
+                if (city == "Sofia")
+                {
+                    commissions = sale * 0.07;
+                }
+                else if (city == "Varna")
+                {
+                    commissions = sale * 0.075;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commissions = sale * 0.08;
+                }
+            }
+
+            else if (1000 < sale && sale <= 10000)
+            {
+                if (city == "Sofia")
+                {
+                    commissions = sale * 0.08;
+                }
+                else if (city == "Varna")
+                {
+                    commissions = sale * 0.10;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commissions = sale * 0.12;
+                }
+            }
+
+            else if (sale > 10000)
+            {
+                if (city == "Sofia")
+                {
+                    commissions = sale * 0.12;
+                }
+                else if (city == "Varna")
+                {
+                    commissions = sale * 0.13;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commissions = sale * 0.145;
+                }
+            }
+
+            if (commissions == 0)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                Console.WriteLine($"{commissions:f2}");
+            }
+        }
+    }
+}
