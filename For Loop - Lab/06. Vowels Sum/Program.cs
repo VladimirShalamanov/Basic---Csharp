8c3df6560a9b32591e67b24c3bb246b944ac988f
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-20 10:54:38 +0300

    Add files via upload

---
+++ For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+            int sum4 = 0;
+            int sum5 = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currenSymbol = text[i];
+
+                if (currenSymbol == 'a')
+                {
+                    sum1++;
+                }
+                else if (currenSymbol == 'e')
+                {
+                    sum2 += 2;
+                }
+                else if (currenSymbol == 'i')
+                {
+                    sum3 += 3;
+                }
+                else if (currenSymbol == 'o')
+                {
+                    sum4 += 4;
+                }
+                else if (currenSymbol == 'u')
+                {
+                    sum5 += 5;
+                }
+            }
+
+            int totalSum = sum1 + sum2 + sum3 + sum4 + sum5;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
