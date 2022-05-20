5679df3cb247691e41fd488b74308f25417cfba3
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-20 10:55:36 +0300

    Add files via upload

---
+++ For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 1; i <= 2 * n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i <= n)
+                {
+                    leftSum += num;
+                }
+                else
+                {
+                    rightSum += num;
+                }
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine($"No, diff = {diff}");
+            }
+        }
+    }
+}
---
+++ For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += num;
+                }
+                else
+                {
+                    oddSum += num;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(oddSum - evenSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
