d99ceb53b8301423b696f4af37466968afa8a4ba
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-22 14:35:24 +0300

    Add files via upload

---
+++ While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < n)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
