fb42529a756caaddfe5fd25b9c13417bfdc0a665
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-23 22:04:33 +0300

    Add files via upload

---
+++ While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int allPicesOfCake = lenght * width;
+
+            while (allPicesOfCake > 0)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    break;
+                }
+
+                int peaceOfCake = int.Parse(input);
+                allPicesOfCake -= peaceOfCake;
+            }
+
+            if (allPicesOfCake >= 0)
+            {
+                Console.WriteLine($"{allPicesOfCake} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(allPicesOfCake)} pieces more.");
+            }
+        }
+    }
+}
---
+++ While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int freeSpace = width * length * height;
+
+            int sumOfCubMeters = 0;
+            string command = Console.ReadLine();
+            while (command != "Done")
+            {
+                int currentCubicMeters = int.Parse(command);
+                sumOfCubMeters += currentCubicMeters;
+                if (freeSpace < sumOfCubMeters)
+                {
+                    int neededCubMeters = sumOfCubMeters - freeSpace;
+                    Console.WriteLine($"No more free space! You need {neededCubMeters} Cubic meters more.");
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "Done")
+            {
+                int freecubMeters = freeSpace - sumOfCubMeters;
+                Console.WriteLine($"{freecubMeters} Cubic meters left.");
+            }
+        }
+    }
+}
