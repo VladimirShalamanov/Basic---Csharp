8844a76345ac54b158fa8343750ac6089eb5b6f2
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-26 14:03:07 +0300

    Add files via upload

---
+++ Nested Loops - Exercise/06. Cinema Tickets/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _06._Cinema_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int totalStudents = 0;
+            int totalStandards = 0;
+            int totalKids = 0;
+            int totalTickets = 0;
+
+            while (command != "Finish")
+            {
+                int students = 0;
+                int standart = 0;
+                int kid = 0;
+                int freeSpots = int.Parse(Console.ReadLine());
+                for (int i = 0; i < freeSpots; i++)
+                {
+                    string TicketType = Console.ReadLine();
+                    if (TicketType == "End")
+                    {
+                        break;
+                    }
+
+                    switch (TicketType)
+                    {
+                        case "student":
+                            students++;
+                            break;
+                        case "standard":
+                            standart++;
+                            break;
+                        case "kid":
+                            kid++;
+                            break;
+                    }
+
+                }
+                totalStudents += students;
+                totalStandards += standart;
+                totalKids += kid;
+                double percentegFull = (students + standart + kid) / (double)freeSpots * 100;
+                Console.WriteLine($"{command} - {percentegFull:f2}% full.");
+                command = Console.ReadLine();
+            }
+
+            totalTickets = totalStudents + totalStandards + totalKids;
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            double studentPercentage = totalStudents / (double) totalTickets * 100;
+            double standartPercentage = totalStandards / (double) totalTickets * 100;
+            double kidsPercentage = totalKids / (double) totalTickets * 100;
+            Console.WriteLine($"{studentPercentage:f2}% student tickets.");
+            Console.WriteLine($"{standartPercentage:f2}% standard tickets.");
+            Console.WriteLine($"{kidsPercentage:f2}% kids tickets.");
+        }
+    }
+}
