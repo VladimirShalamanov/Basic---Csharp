d57afe5219edfa985b5203e9b20846868030b685
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-05-29 17:51:44 +0300

    Add files via upload

---
+++ _Online Preparation Exam - 23 and 24 October 2021/05. Everest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _05._Everest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int count = 1;
+            int start = 5364;
+
+            while (command != "END")
+            {
+                int m = int.Parse(Console.ReadLine());
+
+                if (command == "Yes")
+                {
+                    count++;
+                    start += m;
+                }
+                else if (command == "No")
+                {
+                    count += 0;
+                    start += m;
+                }
+                if (start >= 8848)
+                {
+                    Console.WriteLine($"Goal reached for {count} days!");
+                    break;
+                }
+                else if (5 <= count)
+                {
+                    Console.WriteLine("Failed!");
+                    Console.WriteLine(start);
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "END")
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(start);
+            }
+        }
+    }
+}
---
+++ _Online Preparation Exam - 23 and 24 October 2021/06. Substitute/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06._Substitute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first1start = int.Parse(Console.ReadLine());
+            int first2start = int.Parse(Console.ReadLine());
+            int second1start = int.Parse(Console.ReadLine());
+            int second2start = int.Parse(Console.ReadLine());
+
+            for (int first1 = first1start; first1 <= 8; first1++)
+            {
+                int k = first1;
+                for (int first2 = 9; first2 >= first2start; first2--)
+                {
+                    int l = first2;
+                    if (k % 2 == 0 && l % 2 != 0)
+                    {
+                        for (int num1 = second1start; num1 <= 8; num1++)
+                        {
+                            int m = num1;
+                            for (int num2 = 9; num2 >= second2start; num2--)
+                            {
+                                int n = num2;
+                                if (m % 2 == 0 && n % 2 != 0)
+                                {
+                                    if (k == m && l == n)
+                                    {
+                                        Console.WriteLine("Cannot change the same player.");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"{k}{l} - {m}{n}");
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
