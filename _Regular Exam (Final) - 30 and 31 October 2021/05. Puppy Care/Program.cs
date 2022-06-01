87a16b792cf3f234b868b4addd2ed33ce4fd66c9
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-01 22:12:27 +0300

    Add files via upload

---
+++ _Regular Exam (Final) - 30 and 31 October 2021/05. Puppy Care/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Puppy_Care
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kgFood = int.Parse(Console.ReadLine());
+            int kgToGr = kgFood * 1000;
+            string command = Console.ReadLine();
+            int totalFood = 0;
+
+            while (command != "Adopted")
+            {
+                int grFood = int.Parse(command);
+                totalFood += grFood;
+                
+                command = Console.ReadLine();
+            }
+
+            if (kgToGr >= totalFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {kgToGr-totalFood} grams.");
+            }
+            else if (totalFood > kgToGr)
+            {
+                Console.WriteLine($"Food is not enough. You need {totalFood-kgToGr} grams more.");
+            }
+        }
+    }
+}
---
+++ _Regular Exam (Final) - 30 and 31 October 2021/06. Gold Mine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Gold_Mine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numLocation = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numLocation; i++)
+            {
+                double totalGold = 0;
+                double averageGoldForDay = double.Parse(Console.ReadLine());
+                int workDay = int.Parse(Console.ReadLine());
+
+                for (int j = 1; j <= workDay; j++)
+                {
+                    double kgGold = double.Parse(Console.ReadLine());
+                    totalGold += kgGold;
+                }
+                totalGold /= workDay;
+
+                if (totalGold >= averageGoldForDay)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {totalGold:f2}.");
+                }
+                else if (totalGold < averageGoldForDay)
+                {
+                    Console.WriteLine($"You need {averageGoldForDay - totalGold:f2} gold.");
+                }
+            }
+        }
+    }
+}
